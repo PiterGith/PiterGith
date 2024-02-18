8731710a8a0ab011e20d24df4c0d0106260c71b4
PiterGith <160377574+PiterGith@users.noreply.github.com>
2024-02-18 02:07:42 -0300

    main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+#so this code should force you to put two numbers that added together give 150 or more
 num2 = int(input("cual es tu primer numero: "))
 errapata = int(input("cual es tu segundo numero: "))
 
@@ -17,29 +17,6 @@ while num2 + errapata <= 150:
     num2 = HastaCien(num2) 
     errapata = int(input("Elija otro numero para errapata: "))
     errapata = HastaCien(errapata)
-    print (num2)
-    print(errapata)
-
+    
 print("muito bem :D")
-
-
-
-# def HastaCien(num):
-#     while num >= 100:
-#         num = int(input("Número muy grande, inserte otro: "))
-#     return num
-
-# num2 = int(input("¿Cuál es tu primer número?: "))
-# errapata = int(input("¿Cuál es tu segundo número?: "))
-
-# num2 = HastaCien(num2)
-# errapata = HastaCien(errapata)
-
-# while num2 + errapata <= 150:
-#     print("Tiene que ser más grande que 150")
-#     num2 = HastaCien(num2)
-#     errapata = HastaCien(errapata)
-
-# print("¡Muito bem! :D")
-
-
+print(num2 + errapata)
